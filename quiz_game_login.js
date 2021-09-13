dac27d2311f28700f16fc6abf796299bb45f6385
rituparna-india1947 <85564017+rituparna-india1947@users.noreply.github.com>
2021-09-13 22:44:05 +0530

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,7 @@
+function addUser() {
+    player1_name= document.getElementById("player1_name").value;
+    player2_name= document.getElementById("player2_name").value;
+    localStorage.setItem("player_1", player1_name);
+    localStorage.setItem("player_2", player2_name);
+    window.location= "quiz_game_page.html";
+ }
